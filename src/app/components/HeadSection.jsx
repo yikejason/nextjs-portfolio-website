6309ab085015f6ed1a9d013ac1140e389fa357f5
yikejason <18215513386@163.com>
2023-12-19 15:08:54 +0800

    added head section

---
+++ src/app/components/HeadSection.jsx
@@ -0,0 +1,63 @@
+'use client';
+import React from 'react';
+import Image from 'next/image';
+import { TypeAnimation } from 'react-type-animation';
+
+const HeadSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              {"Hello I'm"}
+            </span>
+            <br />
+            <TypeAnimation
+              sequence={[
+                'Jason',
+                1000,
+                'Front-end Developer',
+                1000,
+                'Back-end Developer',
+                1000,
+                'Full-stack Developer',
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6">
+            This is my portfolio
+          </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 lg:mt-0 bg-white hover:bg-slate-200 text-white">
+              Hire Me
+            </button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 bg-transparent hover:bg-slate-800 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/head-image.png"
+              alt="head image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={500}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeadSection;
+
