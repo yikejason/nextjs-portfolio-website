c8c158c8d489d96a7c8ef12aaadfba030bc17c92
yikejason <18215513386@163.com>
2023-12-25 14:13:43 +0800

    tested email on the vercel

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,24 +1,28 @@
+import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST() {
+export async function POST(req, res) {
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
       from: fromEmail,
-      to: [''],
-      subject: 'Hello world',
+      to: [fromEmail, email],
+      subject: subject,
       react: (
         <>
-          <p>hello jason</p>
+          <h1>{subject}</h1>
+          <p>Thank you for contacting us!</p>
+          <p>New message submitted:</p>
+          <p>{message}</p>
         </>
       ),
     });
-
-    return Response.json(data);
+    return NextResponse.json(data);
   } catch (error) {
-    return Response.json({ error });
+    return NextResponse.json({ error });
   }
 }
-
