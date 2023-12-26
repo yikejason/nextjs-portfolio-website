3518e075d03be81b69af8266da6e957983fb6d1b
yikejason <18215513386@163.com>
2023-12-26 16:06:02 +0800

    updated theme

--- src/app/components/HeadSection.jsx
+++ src/app/components/HeadSection.jsx
@@ -9,7 +9,7 @@ const HeadSection = () => {
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-8 place-self-center text-center sm:text-left just-self-start">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               {"Hello I'm"}
             </span>
             <br />
@@ -33,10 +33,10 @@ const HeadSection = () => {
             This is my portfolio
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 lg:mt-0 bg-white hover:bg-slate-200 text-white">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 via-primary-500 to-secondary-500 lg:mt-0 bg-white hover:bg-slate-200 text-white">
               Hire Me
             </button>
-            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 bg-transparent hover:bg-slate-800 text-white mt-3">
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 via-primary-500 to-secondary-500 bg-transparent hover:bg-slate-800 text-white mt-3">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
--- src/app/components/TabBtn.jsx
+++ src/app/components/TabBtn.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TabBtn = ({ active, selectTab, children }) => {
-  const btnClasses = active ? 'border-b border-purple-500' : 'text-[#ADB7BE]';
+  const btnClasses = active ? 'border-b border-primary-500' : 'text-[#ADB7BE]';
   return (
     <button onClick={selectTab}>
       <p className={`mr-3 font-semibold hover:text-white ${btnClasses}`}>
