c679d5a751a6ed4cafc630272c7d780867c3947d
yikejason <18215513386@163.com>
2023-12-27 14:38:33 +0800

    updated ContactSection style

--- src/app/components/ContactSection.jsx
+++ src/app/components/ContactSection.jsx
@@ -11,7 +11,6 @@ const ContactSection = () => {
       id="contact"
       className="grid md:grid-cols-2 mt-12 mb-24 md:mt-12 md:mb-24 py-24 gap-4 relative"
     >
-      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">
           Let&apos;s Connect
